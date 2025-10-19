4400a6b074e2b5a1f5a79e20cbd19046eeda63d5
begoho <kshitija06@gmail.com>
2025-10-18 18:45:06 -0700

    initial commit

---
+++ src/main/java/com/begoho/BeGoHoJavaSelfTeachingEngineApplication.java
@@ -0,0 +1,13 @@
+package com.begoho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeGoHoJavaSelfTeachingEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeGoHoJavaSelfTeachingEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/begoho/BeGoHoJavaSelfTeachingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.begoho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeGoHoJavaSelfTeachingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
