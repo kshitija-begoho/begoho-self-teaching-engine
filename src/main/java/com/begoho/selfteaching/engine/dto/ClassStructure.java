3f39625e41d3c44d14956a1e85a27a2d8d9924fa
begoho <kshitija06@gmail.com>
2025-11-18 20:16:15 -0700

    adding isMain

--- src/main/java/com/begoho/selfteaching/engine/dto/ClassStructure.java
+++ src/main/java/com/begoho/selfteaching/engine/dto/ClassStructure.java
@@ -4,6 +4,7 @@ public class ClassStructure {
 
     private String accessModifier;
     private String className;
+    private boolean isMain; // new field to indicate whether the class contains a main method
 
     public ClassStructure() {
     }
@@ -11,6 +12,13 @@ public class ClassStructure {
     public ClassStructure(String accessModifier, String className) {
         this.accessModifier = accessModifier;
         this.className = className;
+    }
+
+    // New constructor including isMain
+    public ClassStructure(String accessModifier, String className, boolean isMain) {
+        this.accessModifier = accessModifier;
+        this.className = className;
+        this.isMain = isMain;
     }
 
     public String getAccessModifier() {
@@ -27,5 +35,14 @@ public class ClassStructure {
 
     public void setClassName(String className) {
         this.className = className;
+    }
+
+    // Getter follows JavaBean convention for boolean: isX()
+    public boolean isMain() {
+        return isMain;
+    }
+
+    public void setMain(boolean main) {
+        isMain = main;
     }
 }
