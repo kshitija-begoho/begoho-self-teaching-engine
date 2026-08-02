619dde1e72c1ccd5243a0ebb39b0472f9e57501f
begoho <kshitija06@gmail.com>
2026-08-01 17:55:14 -0700

    backend call integration start

--- frontend/src/JavaFileBuilder.jsx
+++ frontend/src/JavaFileBuilder.jsx
@@ -1,12 +1,53 @@
+import { useState } from 'react'
+
 function JavaFileBuilder()
 {
+const [rows, setRows] = useState([1])
+const[generatedFile, setGeneratedFile] = useState('')
+const [message, setMessage] = useState('')
+const [fileName, setFileName] = useState('')
+
+function addRow()
+{
+setRows([...rows, rows.length + 1])
+}
+
+async function validateAndGenerateJavaFile()
+{
+    const request = {
+         fileName: fileName,
+              structures: [
+                {
+                  accessModifier: 'public',
+                  className: 'First',
+                  isMain: true
+                }
+              ]
+        }
+    const response =  await fetch('/v1/file-structure/generate', {
+                           method: 'POST',
+                           headers: {
+                             'Content-Type': 'application/json'
+                           },
+                           body: JSON.stringify(request)
+                         })
+
+      const result = await response.json()
+      setGeneratedFile(result.generatedCode)
+      setMessage(result.fileName + ' generated successfully')
+
+    }
     return (
         <section>
             <h2> Build Java File </h2>
             <form>
                 <label>Java file name
-                <input/>
+                <input value={fileName} onChange={event => setFileName (event.target.value)}/>
                 </label>
+                  <button type="button" onClick={addRow}>
+                      +
+                    </button>
+                {rows.map(row => (<div key={row}>
                 <label>Access
                 <select>
                     <option>public</option>
@@ -21,9 +62,13 @@ function JavaFileBuilder()
                          <input type="checkbox" />
                          Include main method
                          </label>
-                     <button type="button">Generate Java File</button>
-
+                         </div>
+                        ))}
+                     <button type="button" onClick={validateAndGenerateJavaFile}>Generate Java File</button>
                 </form>
+                <p>{message}</p>
+                <pre>
+                    {generatedFile}</pre>
             </section>
         )
     }
